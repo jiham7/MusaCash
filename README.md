cf6136897a0d5299ae853536bfb46d4e34cc3952
jiham7 <jiham7@gmail.com>
2021-03-30 10:23:09 -0500

    initial Musa

---
+++ README.md
@@ -0,0 +1,46 @@
+<img src="logo.png" alt="MusaCash" height="100px">
+
+## Overview
+
+### Contracts and their addresses on BSC
+
+ * MusaCash.sol - Musa token contract. 
+ * MusaLiquidityMining.sol - Musa liquidity mining contract. 
+ * MusaStaking.sol - Musa staking contract. 
+
+### Installation
+
+Before running any command, make sure to install all dependencies:
+
+```console
+$ npm install
+```
+
+### Test
+
+Run all tests via Truffle:
+
+```console
+$ truffle test
+```
+
+### Compile
+
+Compile all contracts via Truffle:
+
+```console
+$ truffle compile
+```
+
+### Deploy
+
+Deploy on local Truffle Develop blockchain:
+
+```console
+$ truffle develop
+$ migrate
+```
+
+### Learn More
+
+**For full usage information refer to [Truffle documentation](https://www.trufflesuite.com/docs/truffle/overview).**
---
+++ contracts/MusaStaking.sol
@@ -0,0 +1,170 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.6;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+//MUSA.CASH
+contract MusaStaking is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    struct UserInfo {
+        uint256 amount;
+        uint256 rewardDebt;
+        uint256 pendingRewards;
+        uint256 lastClaim;
+    }
+
+    struct PoolInfo {
+        uint256 allocPoint;
+        uint256 lastRewardBlock;
+        uint256 accMusaPerShare;
+        uint256 depositedAmount;
+        uint256 rewardsAmount;
+        uint256 lockupDuration;
+    }
+
+    IERC20 public musa;
+    uint256 public musaPerBlock = uint256(5 ether).div(10); //0.5 MUSA
+
+    PoolInfo[] public poolInfo;
+    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
+    uint256 public totalAllocPoint = 10;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event Claim(address indexed user, uint256 indexed pid, uint256 amount);
+    event ClaimAndStake(address indexed user, uint256 indexed pid, uint256 amount);
+
+    function addPool(uint256 _allocPoint, uint256 _lockupDuration) internal {
+        poolInfo.push(
+            PoolInfo({
+                allocPoint: _allocPoint,
+                lastRewardBlock: 0,
+                accMusaPerShare: 0,
+                depositedAmount: 0,
+                rewardsAmount: 0,
+                lockupDuration: _lockupDuration
+            })
+        );
+    }
+    
+    function setMusaToken(IERC20 _musa) external onlyOwner {
+        require(address(musa) == address(0), 'Token already set!');
+        musa = _musa;
+        addPool(10, 0);
+    }
+    
+    function startStaking(uint256 startBlock) external onlyOwner {
+        require(poolInfo[0].lastRewardBlock == 0, 'Staking already started');
+        poolInfo[0].lastRewardBlock = startBlock;
+    }
+
+    function pendingRewards(uint256 pid, address _user) external view returns (uint256) {
+        require(poolInfo[pid].lastRewardBlock > 0 && block.number >= poolInfo[pid].lastRewardBlock, 'Staking not yet started');
+        PoolInfo storage pool = poolInfo[pid];
+        UserInfo storage user = userInfo[pid][_user];
+        uint256 accMusaPerShare = pool.accMusaPerShare;
+        uint256 depositedAmount = pool.depositedAmount;
+        if (block.number > pool.lastRewardBlock && depositedAmount != 0) {
+            uint256 multiplier = block.number.sub(pool.lastRewardBlock);
+            uint256 musaReward = multiplier.mul(musaPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accMusaPerShare = accMusaPerShare.add(musaReward.mul(1e12).div(depositedAmount));
+        }
+        return user.amount.mul(accMusaPerShare).div(1e12).sub(user.rewardDebt).add(user.pendingRewards);
+    }
+
+    function updatePool(uint256 pid) internal {
+        require(poolInfo[pid].lastRewardBlock > 0 && block.number >= poolInfo[pid].lastRewardBlock, 'Staking not yet started');
+        PoolInfo storage pool = poolInfo[pid];
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 depositedAmount = pool.depositedAmount;
+        if (pool.depositedAmount == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = block.number.sub(pool.lastRewardBlock);
+        uint256 musaReward = multiplier.mul(musaPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        pool.rewardsAmount = pool.rewardsAmount.add(musaReward);
+        pool.accMusaPerShare = pool.accMusaPerShare.add(musaReward.mul(1e12).div(depositedAmount));
+        pool.lastRewardBlock = block.number;
+    }
+
+    function deposit(uint256 pid, uint256 amount) external {
+        PoolInfo storage pool = poolInfo[pid];
+        UserInfo storage user = userInfo[pid][msg.sender];
+        updatePool(pid);
+        if (user.amount > 0) {
+            uint256 pending = user.amount.mul(pool.accMusaPerShare).div(1e12).sub(user.rewardDebt);
+            if (pending > 0) {
+                user.pendingRewards = user.pendingRewards.add(pending);
+            }
+        }
+        if (amount > 0) {
+            musa.transferFrom(address(msg.sender), address(this), amount);
+            user.amount = user.amount.add(amount);
+            pool.depositedAmount = pool.depositedAmount.add(amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accMusaPerShare).div(1e12);
+        user.lastClaim = block.timestamp;
+        emit Deposit(msg.sender, pid, amount);
+    }
+
+    function withdraw(uint256 pid, uint256 amount) external {
+        PoolInfo storage pool = poolInfo[pid];
+        UserInfo storage user = userInfo[pid][msg.sender];
+        require(block.timestamp > user.lastClaim + pool.lockupDuration, "You cannot withdraw yet!");
+        require(user.amount >= amount, "Withdrawing more than you have!");
+        updatePool(pid);
+        uint256 pending = user.amount.mul(pool.accMusaPerShare).div(1e12).sub(user.rewardDebt);
+        if (pending > 0) {
+            user.pendingRewards = user.pendingRewards.add(pending);
+        }
+        if (amount > 0) {
+            musa.safeTransfer(address(msg.sender), amount);
+            user.amount = user.amount.sub(amount);
+            pool.depositedAmount = pool.depositedAmount.sub(amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accMusaPerShare).div(1e12);
+        user.lastClaim = block.timestamp;
+        emit Withdraw(msg.sender, pid, amount);
+    }
+
+    function claim(uint256 pid) public {
+        PoolInfo storage pool = poolInfo[pid];
+        UserInfo storage user = userInfo[pid][msg.sender];
+        updatePool(pid);
+        uint256 pending = user.amount.mul(pool.accMusaPerShare).div(1e12).sub(user.rewardDebt);
+        if (pending > 0 || user.pendingRewards > 0) {
+            user.pendingRewards = user.pendingRewards.add(pending);
+            uint256 claimedAmount = safeMusaTransfer(msg.sender, user.pendingRewards, pid);
+            emit Claim(msg.sender, pid, claimedAmount);
+            user.pendingRewards = user.pendingRewards.sub(claimedAmount);
+            user.lastClaim = block.timestamp;
+            pool.rewardsAmount = pool.rewardsAmount.sub(claimedAmount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accMusaPerShare).div(1e12);
+    }
+    
+    function safeMusaTransfer(address to, uint256 amount, uint256 pid) internal returns (uint256) {
+        PoolInfo memory pool = poolInfo[pid];
+        if (amount > pool.rewardsAmount) {
+            musa.safeTransfer(to, pool.rewardsAmount);
+            return pool.rewardsAmount;
+        } else {
+            musa.safeTransfer(to, amount);
+            return amount;
+        }
+    }
+    
+    function setMusaPerBlock(uint256 _musaPerBlock) external onlyOwner {
+        require(_musaPerBlock > 0, "MUSA per block should be greater than 0!");
+        musaPerBlock = _musaPerBlock;
+    }
+}
